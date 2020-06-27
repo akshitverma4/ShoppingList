9643bb9ee6e23d5a723c82de7504051c8e127cdb
akshitverma4 <akshitverma824@gmail.com>
2020-06-27 11:59:14 +0530

    First Commit

---
+++ app/src/main/java/com/example/shoppinglist/ShoppingRepository.kt
@@ -0,0 +1,11 @@
+package com.example.shoppinglist
+
+class ShoppingRepository(
+    private val db: ShoppingDatabase
+) {
+    suspend fun upsert(item: ShoppingItem) = db.getShoppingDao().upsert(item)
+
+    suspend fun delete(item: ShoppingItem) = db.getShoppingDao().delete(item)
+
+    fun getAllShoppingItems() = db.getShoppingDao().getAllShoppingItems()
+}
